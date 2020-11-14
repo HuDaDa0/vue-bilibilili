fd05b1731197e05af89675d1a1c227bdd7bfebf2
huwei <1497154694@qq.com>
2020-11-14 16:23:12 +0800

    引入vant

---
+++ src/components/common/LoginTop.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login-top">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginTop',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-top {
+  height: 12.5vw;
+  font-size: 4.444vw;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/views/register.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <login-top title="注册bilibilili"></login-top>
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="form.username"
+        name="姓名"
+        label="姓名"
+        placeholder="请输入姓名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+        style="margin: 4.167vw 0"
+      ></van-field>
+      <van-field
+        v-model="form.account"
+        name="账号"
+        label="账号"
+        placeholder="账号"
+        :rules="[{ required: true, message: '请输入账号' }]"
+      ></van-field>
+      <van-field
+        v-model="form.password"
+        name="密码"
+        label="密码"
+        placeholder="请输入密码"
+        :rules="[{ required: true, message: '请输入密码' }]"
+      ></van-field>
+      <div style="padding: 4.167vw 2.778vw;">
+        <van-button
+          round
+          block
+          type="info"
+          native-type="submit"
+          style="background-color: #ff9db5; border-color: #ff9db5;"
+        >
+          注册
+        </van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+import LoginTop from '@/components/common/LoginTop.vue'
+
+export default {
+  name: 'Register',
+  components: {
+    LoginTop
+  },
+  data () {
+    return {
+      form: {
+        username: '',
+        account: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit (value) {
+      console.log(value)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
